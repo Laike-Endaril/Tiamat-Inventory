9810e277b524160b6cbb39edef58f98f30a74669
Kyle Koder <silvertallon@gmail.com>
2019-10-28 20:07:56 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/tiamatrpgmain/TiamatRPGMain.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.tiamatrpgmain;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = TiamatRPGMain.MODID, name = TiamatRPGMain.NAME, version = TiamatRPGMain.VERSION)
+public class TiamatRPGMain
+{
+    public static final String MODID = "tiamatrpgmain";
+    public static final String NAME = "Tiamat RPG - Main";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(TiamatRPGMain.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
