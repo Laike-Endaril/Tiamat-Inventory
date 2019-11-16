71dfa3eef24c6636c90db8f0f0577a6f449544a3
Kyle Koder <silvertallon@gmail.com>
2019-11-16 12:12:07 -0800

    Several texture changes, and replaced the container of the GUI

---
+++ src/main/java/com/fantasticsource/tiamatrpgmain/gui/TiamatInventoryContainer.java
@@ -0,0 +1,212 @@
+package com.fantasticsource.tiamatrpgmain.gui;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.*;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+
+public class TiamatInventoryContainer extends Container
+{
+    private static final EntityEquipmentSlot[] VALID_EQUIPMENT_SLOTS = new EntityEquipmentSlot[]{EntityEquipmentSlot.HEAD, EntityEquipmentSlot.CHEST, EntityEquipmentSlot.LEGS, EntityEquipmentSlot.FEET};
+    public InventoryCrafting craftMatrix = new InventoryCrafting(this, 2, 2);
+    public InventoryCraftResult craftResult = new InventoryCraftResult();
+    public boolean isLocalWorld;
+    private final EntityPlayer player;
+
+    public TiamatInventoryContainer(EntityPlayer playerIn)
+    {
+        player = playerIn;
+        InventoryPlayer playerInventory = playerIn.inventory;
+
+        isLocalWorld = !playerIn.world.isRemote;
+        addSlotToContainer(new SlotCrafting(playerInventory.player, craftMatrix, craftResult, 0, 154, 28));
+
+        for (int i = 0; i < 2; ++i)
+        {
+            for (int j = 0; j < 2; ++j)
+            {
+                addSlotToContainer(new Slot(craftMatrix, j + i * 2, 98 + j * 18, 18 + i * 18));
+            }
+        }
+
+        for (int k = 0; k < 4; ++k)
+        {
+            final EntityEquipmentSlot entityequipmentslot = VALID_EQUIPMENT_SLOTS[k];
+            addSlotToContainer(new Slot(playerInventory, 36 + (3 - k), 8, 8 + k * 18)
+            {
+                public int getSlotStackLimit()
+                {
+                    return 1;
+                }
+
+                public boolean isItemValid(ItemStack stack)
+                {
+                    return stack.getItem().isValidArmor(stack, entityequipmentslot, player);
+                }
+
+                public boolean canTakeStack(EntityPlayer playerIn)
+                {
+                    ItemStack itemstack = getStack();
+                    return !itemstack.isEmpty() && !playerIn.isCreative() && EnchantmentHelper.hasBindingCurse(itemstack) ? false : super.canTakeStack(playerIn);
+                }
+
+                @Nullable
+                @SideOnly(Side.CLIENT)
+                public String getSlotTexture()
+                {
+                    return ItemArmor.EMPTY_SLOT_NAMES[entityequipmentslot.getIndex()];
+                }
+            });
+        }
+
+        for (int l = 0; l < 3; ++l)
+        {
+            for (int j1 = 0; j1 < 9; ++j1)
+            {
+                addSlotToContainer(new Slot(playerInventory, j1 + (l + 1) * 9, 8 + j1 * 18, 84 + l * 18));
+            }
+        }
+
+        for (int i1 = 0; i1 < 9; ++i1)
+        {
+            addSlotToContainer(new Slot(playerInventory, i1, 8 + i1 * 18, 142));
+        }
+
+        addSlotToContainer(new Slot(playerInventory, 40, 77, 62)
+        {
+            @Nullable
+            @SideOnly(Side.CLIENT)
+            public String getSlotTexture()
+            {
+                return "minecraft:items/empty_armor_slot_shield";
+            }
+        });
+    }
+
+    public void onCraftMatrixChanged(IInventory inventoryIn)
+    {
+        slotChangedCraftingGrid(player.world, player, craftMatrix, craftResult);
+    }
+
+    public void onContainerClosed(EntityPlayer playerIn)
+    {
+        super.onContainerClosed(playerIn);
+        craftResult.clear();
+
+        if (!playerIn.world.isRemote)
+        {
+            clearContainer(playerIn, playerIn.world, craftMatrix);
+        }
+    }
+
+    public boolean canInteractWith(EntityPlayer playerIn)
+    {
+        return true;
+    }
+
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
+    {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+            EntityEquipmentSlot entityequipmentslot = EntityLiving.getSlotForItemStack(itemstack);
+
+            if (index == 0)
+            {
+                if (!mergeItemStack(itemstack1, 9, 45, true))
+                {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onSlotChange(itemstack1, itemstack);
+            }
+            else if (index < 5)
+            {
+                if (!mergeItemStack(itemstack1, 9, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (index < 9)
+            {
+                if (!mergeItemStack(itemstack1, 9, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (entityequipmentslot.getSlotType() == EntityEquipmentSlot.Type.ARMOR && !inventorySlots.get(8 - entityequipmentslot.getIndex()).getHasStack())
+            {
+                int i = 8 - entityequipmentslot.getIndex();
+
+                if (!mergeItemStack(itemstack1, i, i + 1, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (entityequipmentslot == EntityEquipmentSlot.OFFHAND && !inventorySlots.get(45).getHasStack())
+            {
+                if (!mergeItemStack(itemstack1, 45, 46, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (index < 36)
+            {
+                if (!mergeItemStack(itemstack1, 36, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (index < 45)
+            {
+                if (!mergeItemStack(itemstack1, 9, 36, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (!mergeItemStack(itemstack1, 9, 45, false))
+            {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty())
+            {
+                slot.putStack(ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            ItemStack itemstack2 = slot.onTake(playerIn, itemstack1);
+
+            if (index == 0)
+            {
+                playerIn.dropItem(itemstack2, false);
+            }
+        }
+
+        return itemstack;
+    }
+
+    public boolean canMergeSlot(ItemStack stack, Slot slotIn)
+    {
+        return slotIn.inventory != craftResult && super.canMergeSlot(stack, slotIn);
+    }
+}
--- src/main/java/com/fantasticsource/tiamatrpgmain/gui/TiamatInventoryGUI.java
+++ src/main/java/com/fantasticsource/tiamatrpgmain/gui/TiamatInventoryGUI.java
@@ -4,18 +4,21 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiButtonImage;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static com.fantasticsource.tiamatrpgmain.Keys.TIAMAT_INVENTORY_KEY;
 import static com.fantasticsource.tiamatrpgmain.TiamatRPGMain.MODID;
 
+@SideOnly(Side.CLIENT)
 public class TiamatInventoryGUI extends GuiContainer
 {
     private static int tab = 0;
@@ -25,9 +28,9 @@ public class TiamatInventoryGUI extends GuiContainer
 
     private int uOffset, vOffset;
 
-    public TiamatInventoryGUI(EntityPlayer player)
+    public TiamatInventoryGUI()
     {
-        super(player.inventoryContainer);
+        super(new TiamatInventoryContainer(Minecraft.getMinecraft().player));
         allowUserInput = true;
     }
 
@@ -62,47 +65,7 @@ public class TiamatInventoryGUI extends GuiContainer
         bufferbuilder.pos(guiLeft, guiTop, zLevel).tex(uOffset * U_PIXEL, vOffset * V_PIXEL).endVertex();
         tessellator.draw();
 
-        drawEntityOnScreen(guiLeft + 60, guiTop + 51 + 30, 30, 0, 0, mc.player);
-    }
-
-    public static void drawEntityOnScreen(int posX, int posY, int scale, float mouseX, float mouseY, EntityLivingBase ent)
-    {
-        GlStateManager.enableColorMaterial();
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(posX, posY, 50);
-        GlStateManager.scale(-scale, scale, scale);
-        GlStateManager.rotate(180, 0, 0, 1);
-        float f = ent.renderYawOffset;
-        float f1 = ent.rotationYaw;
-        float f2 = ent.rotationPitch;
-        float f3 = ent.prevRotationYawHead;
-        float f4 = ent.rotationYawHead;
-        GlStateManager.rotate(135, 0, 1, 0);
-        RenderHelper.enableStandardItemLighting();
-        GlStateManager.rotate(-135, 0, 1, 0);
-        GlStateManager.rotate(-((float) Math.atan((double) (mouseY / 40))) * 20, 1, 0, 0);
-        ent.renderYawOffset = (float) Math.atan((double) (mouseX / 40)) * 20;
-        ent.rotationYaw = (float) Math.atan((double) (mouseX / 40)) * 40;
-        ent.rotationPitch = -((float) Math.atan((double) (mouseY / 40))) * 20;
-        ent.rotationYawHead = ent.rotationYaw;
-        ent.prevRotationYawHead = ent.rotationYaw;
-        GlStateManager.translate(0, 0, 0);
-        RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
-        rendermanager.setPlayerViewY(180);
-        rendermanager.setRenderShadow(false);
-        rendermanager.renderEntity(ent, 0, 0, 0, 0, 1, false);
-        rendermanager.setRenderShadow(true);
-        ent.renderYawOffset = f;
-        ent.rotationYaw = f1;
-        ent.rotationPitch = f2;
-        ent.prevRotationYawHead = f3;
-        ent.rotationYawHead = f4;
-        GlStateManager.popMatrix();
-        RenderHelper.disableStandardItemLighting();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
-        GlStateManager.disableTexture2D();
-        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+        GuiInventory.drawEntityOnScreen(guiLeft + 60, guiTop + 66 + 30, 30, 0, 0, mc.player);
     }
 
     protected void mouseReleased(int mouseX, int mouseY, int state)
@@ -117,7 +80,7 @@ public class TiamatInventoryGUI extends GuiContainer
     {
         if (TIAMAT_INVENTORY_KEY.isPressed() && TIAMAT_INVENTORY_KEY.getKeyConflictContext().isActive())
         {
-            Minecraft.getMinecraft().displayGuiScreen(new TiamatInventoryGUI(Minecraft.getMinecraft().player));
+            Minecraft.getMinecraft().displayGuiScreen(new TiamatInventoryGUI());
         }
     }
 
@@ -130,22 +93,22 @@ public class TiamatInventoryGUI extends GuiContainer
         switch (tab)
         {
             case 0:
-                xSize = 249;
-                ySize = 197;
+                xSize = 250;
+                ySize = 214;
                 uOffset = 0;
                 vOffset = 0;
                 break;
 
             case 1:
-                xSize = 249;
-                ySize = 197;
+                xSize = 250;
+                ySize = 214;
                 uOffset = 0;
                 vOffset = 256;
                 break;
 
             case 2:
-                xSize = 249;
-                ySize = 197;
+                xSize = 250;
+                ySize = 214;
                 uOffset = 256;
                 vOffset = 0;
                 break;
@@ -161,6 +124,9 @@ public class TiamatInventoryGUI extends GuiContainer
         buttonList.add(button);
         button = new GuiButtonImage(2, guiLeft + 230, guiTop + 68, 19, 31, TEXTURE_W - 19, TEXTURE_H - 31, 0, TEXTURE);
         buttonList.add(button);
+
+        //Container
+        inventorySlots = new TiamatInventoryContainer(Minecraft.getMinecraft().player);
     }
 
     protected void actionPerformed(GuiButton button)
