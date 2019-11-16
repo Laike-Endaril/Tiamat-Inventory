5efeef3aebd99ed8178bf53091a3ee1336dbdd2d
Kyle Koder <silvertallon@gmail.com>
2019-11-16 15:33:59 -0800

    Armourer's Workshop compat

--- src/main/java/com/fantasticsource/tiamatrpgmain/gui/TiamatInventoryGUI.java
+++ src/main/java/com/fantasticsource/tiamatrpgmain/gui/TiamatInventoryGUI.java
@@ -1,5 +1,7 @@
 package com.fantasticsource.tiamatrpgmain.gui;
 
+import moe.plushie.armourers_workshop.common.network.PacketHandler;
+import moe.plushie.armourers_workshop.common.network.messages.client.MessageClientKeyPress;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiButtonImage;
@@ -12,6 +14,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -25,6 +28,7 @@ public class TiamatInventoryGUI extends GuiContainer
     private static final ResourceLocation TEXTURE = new ResourceLocation(MODID, "gui/inventory.png");
     private static final int TEXTURE_W = 512, TEXTURE_H = 512;
     private static final double U_PIXEL = 1d / TEXTURE_W, V_PIXEL = 1d / TEXTURE_H;
+    private static boolean reopen = false;
 
     private int uOffset, vOffset;
 
@@ -124,6 +128,8 @@ public class TiamatInventoryGUI extends GuiContainer
         buttonList.add(button);
         button = new GuiButtonImage(2, guiLeft + 230, guiTop + 68, 19, 31, TEXTURE_W - 19, TEXTURE_H - 31, 0, TEXTURE);
         buttonList.add(button);
+        button = new GuiButtonImage(3, guiLeft + 230, guiTop + 100, 19, 31, TEXTURE_W - 19, TEXTURE_H - 31, 0, TEXTURE);
+        buttonList.add(button);
 
         //Container
         inventorySlots = new TiamatInventoryContainer(Minecraft.getMinecraft().player);
@@ -134,5 +140,20 @@ public class TiamatInventoryGUI extends GuiContainer
         buttonClicked = true;
 
         if (button.id <= 2) setTab(button.id);
+        else if (button.id == 3)
+        {
+            reopen = true;
+            PacketHandler.networkWrapper.sendToServer(new MessageClientKeyPress(MessageClientKeyPress.Button.OPEN_WARDROBE));
+        }
+    }
+
+    @SubscribeEvent
+    public static void clientTick(TickEvent.ClientTickEvent event)
+    {
+        if (reopen && Minecraft.getMinecraft().currentScreen == null)
+        {
+            reopen = false;
+            Minecraft.getMinecraft().displayGuiScreen(new TiamatInventoryGUI());
+        }
     }
 }
