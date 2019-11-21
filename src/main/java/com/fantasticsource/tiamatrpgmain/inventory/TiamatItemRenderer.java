4e86765825681a7d647088aebad04981bfa20c5d
Kyle Koder <silvertallon@gmail.com>
2019-11-21 11:03:49 -0800

    Saving custom item renderer for later; working on attributes (stats) first

---
+++ src/main/java/com/fantasticsource/tiamatrpgmain/inventory/TiamatItemRenderer.java
@@ -0,0 +1,15 @@
+package com.fantasticsource.tiamatrpgmain.inventory;
+
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ModelManager;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.renderer.texture.TextureManager;
+
+public class TiamatItemRenderer extends RenderItem
+{
+    //TODO
+    public TiamatItemRenderer(TextureManager p_i46552_1_, ModelManager p_i46552_2_, ItemColors p_i46552_3_)
+    {
+        super(p_i46552_1_, p_i46552_2_, p_i46552_3_);
+    }
+}
