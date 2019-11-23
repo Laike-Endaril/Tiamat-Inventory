a240753f0f6cd6929f5e21d31598bb015f7291a8
Kyle Koder <silvertallon@gmail.com>
2019-11-22 16:01:37 -0800

    Changed name to just Tiamat RPG

--- src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
+++ src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 public class TiamatRPG
 {
     public static final String MODID = "tiamatrpg";
-    public static final String NAME = "Tiamat RPG - Main";
+    public static final String NAME = "Tiamat RPG";
     public static final String VERSION = "1.12.2.000";
 
     public TiamatRPG()
