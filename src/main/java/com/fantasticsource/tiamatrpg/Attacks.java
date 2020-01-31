967ef14dbe2aa197b00ddf2787aa1bed6a09d887
Kyle Koder <silvertallon@gmail.com>
2020-01-30 16:49:36 -0800

    Gutted the static attributes, but left some commented code here and there for future reference

--- src/main/java/com/fantasticsource/tiamatrpg/Attacks.java
+++ src/main/java/com/fantasticsource/tiamatrpg/Attacks.java
@@ -2,22 +2,15 @@ package com.fantasticsource.tiamatrpg;
 
 import com.fantasticsource.mctools.MCTools;
 import com.fantasticsource.tools.ReflectionTool;
-import com.fantasticsource.tools.datastructures.ExplicitPriorityQueue;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import scala.actors.threadpool.Arrays;
 
 import java.lang.reflect.Field;
-import java.util.List;
 
-import static com.fantasticsource.tiamatrpg.Attributes.*;
 import static net.minecraft.entity.SharedMonsterAttributes.KNOCKBACK_RESISTANCE;
 
 public class Attacks
@@ -26,61 +19,62 @@ public class Attacks
     public static boolean tiamatAttackActive = false;
 
 
-    public static void tiamatAttack(EntityPlayerMP attacker, Class filter) throws IllegalAccessException
-    {
-        //Possible hit count
-        double hitsRemaining = MCTools.getAttribute(attacker, MELEE_TARGETS);
-        if (hitsRemaining <= 0) return;
-
-
-        //Cube distance check
-        double range = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE) + MCTools.getAttribute(attacker, MELEE_TOLERANCE);
-        List<Entity> entityList = attacker.world.getEntitiesWithinAABBExcludingEntity(attacker, attacker.getEntityBoundingBox().grow(range));
-        if (entityList.size() == 0) return;
-
-
-        double angle = MCTools.getAttribute(attacker, MELEE_ANGLE);
-        Vec3d playerEyes = attacker.getPositionVector().addVector(0, attacker.eyeHeight, 0);
-        ExplicitPriorityQueue<Entity> queue = new ExplicitPriorityQueue<>(entityList.size());
-
-        //Filter check
-        entityList.removeIf(target -> filter != null && !filter.isAssignableFrom(target.getClass()));
-
-        //Cone check
-        entityList = Arrays.asList(MCTools.withinCone(playerEyes, attacker.rotationYawHead, attacker.rotationPitch, range, angle, true, entityList.toArray(new Entity[0])));
-        if (entityList.size() == 0) return;
-
-
-        //Attack sequence
-        int lastSwingTime = (int) entityLivingBaseTicksSinceLastSwingField.get(attacker);
-        while (queue.size() > 0)
-        {
-            Entity target = queue.poll();
-
-
-            if (target instanceof EntityLivingBase)
-            {
-                //Block
-                if (false)
-                {
-                    //TODO make knockback use damage amount (need to do damage calcs first!)
-                    knockback((EntityLivingBase) target, attacker, 100);
-                    //TODO particle effect
-
-                    //Stop the attack
-                    break;
-                }
-            }
-
-            tiamatAttackActive = true;
-            attacker.attackTargetEntityWithCurrentItem(target);
-            tiamatAttackActive = false;
-
-            entityLivingBaseTicksSinceLastSwingField.set(attacker, lastSwingTime);
-            if (--hitsRemaining <= 0) break;
-        }
-        attacker.resetCooldown();
-    }
+    //TODO old code for cone attacks
+//    public static void tiamatAttack(EntityPlayerMP attacker, Class filter) throws IllegalAccessException
+//    {
+//        //Possible hit count
+//        double hitsRemaining = MCTools.getAttribute(attacker, MELEE_TARGETS);
+//        if (hitsRemaining <= 0) return;
+//
+//
+//        //Cube distance check
+//        double range = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE) + MCTools.getAttribute(attacker, MELEE_TOLERANCE);
+//        List<Entity> entityList = attacker.world.getEntitiesWithinAABBExcludingEntity(attacker, attacker.getEntityBoundingBox().grow(range));
+//        if (entityList.size() == 0) return;
+//
+//
+//        double angle = MCTools.getAttribute(attacker, MELEE_ANGLE);
+//        Vec3d playerEyes = attacker.getPositionVector().addVector(0, attacker.eyeHeight, 0);
+//        ExplicitPriorityQueue<Entity> queue = new ExplicitPriorityQueue<>(entityList.size());
+//
+//        //Filter check
+//        entityList.removeIf(target -> filter != null && !filter.isAssignableFrom(target.getClass()));
+//
+//        //Cone check
+//        entityList = Arrays.asList(MCTools.withinCone(playerEyes, attacker.rotationYawHead, attacker.rotationPitch, range, angle, true, entityList.toArray(new Entity[0])));
+//        if (entityList.size() == 0) return;
+//
+//
+//        //Attack sequence
+//        int lastSwingTime = (int) entityLivingBaseTicksSinceLastSwingField.get(attacker);
+//        while (queue.size() > 0)
+//        {
+//            Entity target = queue.poll();
+//
+//
+//            if (target instanceof EntityLivingBase)
+//            {
+//                //Block
+//                if (false)
+//                {
+//                    //TODO make knockback use damage amount (need to do damage calcs first!)
+//                    knockback((EntityLivingBase) target, attacker, 100);
+//                    //TODO particle effect
+//
+//                    //Stop the attack
+//                    break;
+//                }
+//            }
+//
+//            tiamatAttackActive = true;
+//            attacker.attackTargetEntityWithCurrentItem(target);
+//            tiamatAttackActive = false;
+//
+//            entityLivingBaseTicksSinceLastSwingField.set(attacker, lastSwingTime);
+//            if (--hitsRemaining <= 0) break;
+//        }
+//        attacker.resetCooldown();
+//    }
 
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -120,52 +114,53 @@ public class Attacks
     }
 
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void damageEntity(LivingHurtEvent event)
-    {
-        DamageSource source = event.getSource();
-        Entity entity = source.getTrueSource();
-        if (!(entity instanceof EntityLivingBase)) entity = source.getImmediateSource();
-        if (!(entity instanceof EntityLivingBase) || entity.world.isRemote) return;
-
-        EntityLivingBase attacker = (EntityLivingBase) entity;
-        switch ((int) MCTools.getAttribute(attacker, MELEE_MODE))
-        {
-            case 1:
-            {
-                double dist = attacker.getDistance(event.getEntityLiving());
-                double optimum = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE);
-                double dif = Math.abs(dist - optimum);
-                if (dif > MCTools.getAttribute(attacker, MELEE_TOLERANCE))
-                {
-                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD)));
-                }
-                else
-                {
-                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_GOOD)));
-                }
-                break;
-            }
-
-            case 2:
-            {
-                double dist = attacker.getDistance(event.getEntityLiving());
-                double optimum = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE);
-                double dif = Math.abs(dist - optimum);
-                double tolerance = MCTools.getAttribute(attacker, MELEE_TOLERANCE);
-                if (dif > tolerance)
-                {
-                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD)));
-                }
-                else
-                {
-                    double ratio = 1 - (dif / tolerance);
-                    double multiplier = MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD) + MCTools.getAttribute(attacker, MELEE_MULTIPLIER_GOOD) * ratio;
-                    event.setAmount((float) (event.getAmount() * multiplier));
-                }
-                break;
-            }
-        }
-        //If mode is not set, just do full damage at all distances in range
-    }
+    //TODO old code for altering damage
+//    @SubscribeEvent(priority = EventPriority.HIGHEST)
+//    public static void damageEntity(LivingHurtEvent event)
+//    {
+//        DamageSource source = event.getSource();
+//        Entity entity = source.getTrueSource();
+//        if (!(entity instanceof EntityLivingBase)) entity = source.getImmediateSource();
+//        if (!(entity instanceof EntityLivingBase) || entity.world.isRemote) return;
+//
+//        EntityLivingBase attacker = (EntityLivingBase) entity;
+//        switch ((int) MCTools.getAttribute(attacker, MELEE_MODE))
+//        {
+//            case 1:
+//            {
+//                double dist = attacker.getDistance(event.getEntityLiving());
+//                double optimum = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE);
+//                double dif = Math.abs(dist - optimum);
+//                if (dif > MCTools.getAttribute(attacker, MELEE_TOLERANCE))
+//                {
+//                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD)));
+//                }
+//                else
+//                {
+//                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_GOOD)));
+//                }
+//                break;
+//            }
+//
+//            case 2:
+//            {
+//                double dist = attacker.getDistance(event.getEntityLiving());
+//                double optimum = MCTools.getAttribute(attacker, MELEE_BEST_DISTANCE);
+//                double dif = Math.abs(dist - optimum);
+//                double tolerance = MCTools.getAttribute(attacker, MELEE_TOLERANCE);
+//                if (dif > tolerance)
+//                {
+//                    event.setAmount((float) (event.getAmount() * MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD)));
+//                }
+//                else
+//                {
+//                    double ratio = 1 - (dif / tolerance);
+//                    double multiplier = MCTools.getAttribute(attacker, MELEE_MULTIPLIER_BAD) + MCTools.getAttribute(attacker, MELEE_MULTIPLIER_GOOD) * ratio;
+//                    event.setAmount((float) (event.getAmount() * multiplier));
+//                }
+//                break;
+//            }
+//        }
+//        //If mode is not set, just do full damage at all distances in range
+//    }
 }
--- src/main/java/com/fantasticsource/tiamatrpg/Attributes.java
+++ src/main/java/com/fantasticsource/tiamatrpg/Attributes.java
@@ -3,46 +3,22 @@ package com.fantasticsource.tiamatrpg;
 import com.fantasticsource.tiamatrpg.config.TiamatConfig;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeMap;
-import net.minecraft.entity.ai.attributes.RangedAttribute;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-import static com.fantasticsource.tiamatrpg.TiamatRPG.MODID;
 
 public class Attributes
 {
     public static String[] displayAttributes = new String[0], displayAttributeDescriptions = new String[0];
 
-    public static final double
-            DEFAULT_MELEE_ANGLE = 0,
-            DEFAULT_MELEE_TARGETS = 1,
-            DEFAULT_MELEE_MODE = 0, //0 means normal damage anywhere in tolerance; 1 means good mult anywhere in tolerance, bad mult outside; 2 means fade from good mult at best distance to bad mult at edges of tolerance, and bad mult outside tolerance
-            DEFAULT_MELEE_BEST_DISTANCE = 1.5,
-            DEFAULT_MELEE_TOLERANCE = 1.5,
-            DEFAULT_MELEE_MULTIPLIER_GOOD = 1,
-            DEFAULT_MELEE_MULTIPLIER_BAD = 0;
+//    public static final double DEFAULT_MELEE_ANGLE = 0;
 
-    public static RangedAttribute
-            MELEE_ANGLE,
-            MELEE_TARGETS,
-            MELEE_MODE,
-            MELEE_BEST_DISTANCE,
-            MELEE_TOLERANCE,
-            MELEE_MULTIPLIER_GOOD,
-            MELEE_MULTIPLIER_BAD;
+//    public static RangedAttribute MELEE_ANGLE;
 
     public static void init()
     {
-        MELEE_ANGLE = new RangedAttribute(null, MODID + ".meleeAngle", DEFAULT_MELEE_ANGLE, 0, Double.MAX_VALUE);
-        MELEE_TARGETS = new RangedAttribute(null, MODID + ".meleeTargets", DEFAULT_MELEE_TARGETS, 0, Double.MAX_VALUE);
-        MELEE_MODE = new RangedAttribute(null, MODID + ".meleeMode", DEFAULT_MELEE_MODE, 0, Double.MAX_VALUE);
-        MELEE_BEST_DISTANCE = new RangedAttribute(null, MODID + ".meleeBestDistance", DEFAULT_MELEE_BEST_DISTANCE, 0, Double.MAX_VALUE);
-        MELEE_TOLERANCE = new RangedAttribute(null, MODID + ".meleeTolerance", DEFAULT_MELEE_TOLERANCE, 0, Double.MAX_VALUE);
-        MELEE_MULTIPLIER_GOOD = new RangedAttribute(null, MODID + ".meleeMultiplierGood", DEFAULT_MELEE_MULTIPLIER_GOOD, 0, Double.MAX_VALUE);
-        MELEE_MULTIPLIER_BAD = new RangedAttribute(null, MODID + ".meleeMultiplierBad", DEFAULT_MELEE_MULTIPLIER_BAD, 0, Double.MAX_VALUE);
+//        MELEE_ANGLE = new RangedAttribute(null, MODID + ".meleeAngle", DEFAULT_MELEE_ANGLE, 0, Double.MAX_VALUE);
     }
-
 
     public static void addAttributes(EntityLivingBase livingBase)
     {
@@ -51,14 +27,7 @@ public class Attributes
             AttributeMap attributeMap = (AttributeMap) livingBase.getAttributeMap();
 
             //Add new attributes to entity
-            attributeMap.registerAttribute(MELEE_ANGLE);
-            attributeMap.registerAttribute(MELEE_TARGETS);
-
-            attributeMap.registerAttribute(MELEE_MODE);
-            attributeMap.registerAttribute(MELEE_BEST_DISTANCE);
-            attributeMap.registerAttribute(MELEE_TOLERANCE);
-            attributeMap.registerAttribute(MELEE_MULTIPLIER_GOOD);
-            attributeMap.registerAttribute(MELEE_MULTIPLIER_BAD);
+//            attributeMap.registerAttribute(MELEE_ANGLE);
         }
     }
 
@@ -68,15 +37,8 @@ public class Attributes
         {
             AttributeMap attributeMap = (AttributeMap) livingBase.getAttributeMap();
 
-            //Add new attributes to entity
-            attributeMap.getAttributeInstance(MELEE_ANGLE).setBaseValue(MELEE_ANGLE.getDefaultValue());
-            attributeMap.getAttributeInstance(MELEE_TARGETS).setBaseValue(MELEE_TARGETS.getDefaultValue());
-
-            attributeMap.getAttributeInstance(MELEE_MODE).setBaseValue(MELEE_MODE.getDefaultValue());
-            attributeMap.getAttributeInstance(MELEE_BEST_DISTANCE).setBaseValue(MELEE_BEST_DISTANCE.getDefaultValue());
-            attributeMap.getAttributeInstance(MELEE_TOLERANCE).setBaseValue(MELEE_TOLERANCE.getDefaultValue());
-            attributeMap.getAttributeInstance(MELEE_MULTIPLIER_GOOD).setBaseValue(MELEE_MULTIPLIER_GOOD.getDefaultValue());
-            attributeMap.getAttributeInstance(MELEE_MULTIPLIER_BAD).setBaseValue(MELEE_MULTIPLIER_BAD.getDefaultValue());
+            //Add edit entity's base values for attributes
+//            attributeMap.getAttributeInstance(MELEE_ANGLE).setBaseValue(MELEE_ANGLE.getDefaultValue());
         }
     }
 
