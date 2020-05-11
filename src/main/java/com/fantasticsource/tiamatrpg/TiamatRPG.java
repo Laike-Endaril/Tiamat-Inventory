a79526f2ebcabeca8896bf3c07d9bf08d18c7703
Kyle Koder <silvertallon@gmail.com>
2020-05-11 07:47:04 -0700

    1.12.2.000f

--- src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
+++ src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
@@ -28,7 +28,7 @@ public class TiamatRPG
 {
     public static final String MODID = "tiamatrpg";
     public static final String NAME = "Tiamat RPG";
-    public static final String VERSION = "1.12.2.000e";
+    public static final String VERSION = "1.12.2.000f";
 
 
     @Mod.EventHandler
