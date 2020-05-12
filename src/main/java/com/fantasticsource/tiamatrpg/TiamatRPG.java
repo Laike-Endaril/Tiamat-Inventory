f7b37312ec3052cb789d5a9e6096db4361851b70
Kyle Koder <silvertallon@gmail.com>
2020-05-12 14:35:04 -0700

    Fixed incorrect flib version req

--- src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
+++ src/main/java/com/fantasticsource/tiamatrpg/TiamatRPG.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = TiamatRPG.MODID, name = TiamatRPG.NAME, version = TiamatRPG.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034q,);required-after:tiamatitems@[1.12.2.000b,);required-after:tiamatactions@[1.12.2.000,)")
+@Mod(modid = TiamatRPG.MODID, name = TiamatRPG.NAME, version = TiamatRPG.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034r,);required-after:tiamatitems@[1.12.2.000b,);required-after:tiamatactions@[1.12.2.000,)")
 public class TiamatRPG
 {
     public static final String MODID = "tiamatrpg";
