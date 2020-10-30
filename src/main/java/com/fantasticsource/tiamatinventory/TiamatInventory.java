37266e053c49a1a4161578c84dc462e6095ad13e
Kyle Koder <silvertallon@gmail.com>
2020-10-30 13:05:22 -0700

    Updated libs / reqs to fix issue with tiamat items not applying their passive attribute mods

--- src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
+++ src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = TiamatInventory.MODID, name = TiamatInventory.NAME, version = TiamatInventory.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.039a,);required-after:tiamatitems@[1.12.2.000b,);required-after:tiamatactions@[1.12.2.000,)")
+@Mod(modid = TiamatInventory.MODID, name = TiamatInventory.NAME, version = TiamatInventory.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.039a,);required-after:tiamatitems@[1.12.2.000zg,);required-after:tiamatactions@[1.12.2.000s,)")
 public class TiamatInventory
 {
     public static final String MODID = "tiamatinventory";
