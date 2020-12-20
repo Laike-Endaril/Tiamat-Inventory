d2e605fb661bdedce3142fe471b167bd890e94f5
Kyle Koder <silvertallon@gmail.com>
2020-12-19 20:12:34 -0800

    1.12.2.000zr

--- src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
+++ src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
@@ -43,7 +43,7 @@ public class TiamatInventory
 {
     public static final String MODID = "tiamatinventory";
     public static final String NAME = "Tiamat Inventory";
-    public static final String VERSION = "1.12.2.000zq";
+    public static final String VERSION = "1.12.2.000zr";
 
 
     @Mod.EventHandler
