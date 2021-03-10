fd00b1183f275408c007cffb3218b770addd2de5
Kyle Koder <silvertallon@gmail.com>
2021-03-10 11:42:22 -0800

    1.12.2.000zw

--- src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
+++ src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
@@ -42,12 +42,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = TiamatInventory.MODID, name = TiamatInventory.NAME, version = TiamatInventory.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044v,)")
+@Mod(modid = TiamatInventory.MODID, name = TiamatInventory.NAME, version = TiamatInventory.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044zk,)")
 public class TiamatInventory
 {
     public static final String MODID = "tiamatinventory";
     public static final String NAME = "Tiamat Inventory";
-    public static final String VERSION = "1.12.2.000zv";
+    public static final String VERSION = "1.12.2.000zw";
 
 
     @CapabilityInject(ICurrencyCapability.class)
