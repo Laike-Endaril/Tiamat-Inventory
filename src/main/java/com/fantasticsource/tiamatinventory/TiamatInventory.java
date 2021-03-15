2b6b3ecba51d1e6a4cf73b799e355ca45d1e2e70
Kyle Koder <silvertallon@gmail.com>
2021-03-15 14:05:08 -0700

    1.12.2.000zzb

--- src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
+++ src/main/java/com/fantasticsource/tiamatinventory/TiamatInventory.java
@@ -48,7 +48,7 @@ public class TiamatInventory
 {
     public static final String MODID = "tiamatinventory";
     public static final String NAME = "Tiamat Inventory";
-    public static final String VERSION = "1.12.2.000zza";
+    public static final String VERSION = "1.12.2.000zzb";
 
 
     @CapabilityInject(ICurrencyCapability.class)
